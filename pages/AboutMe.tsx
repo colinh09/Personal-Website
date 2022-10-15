24024c7017b97377a37988b712b28f53ab7e9ae3
colinh09 <colin.hwang@cooper.edu>
2022-10-14 23:03:10 -0400

    please please please

--- pages/AboutMe.tsx
+++ pages/AboutMe.tsx
@@ -12,13 +12,13 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
     return (
-        <div className = "overflow-y-scroll">
+        <div>
             {/* <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
                 <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
                     ABOUT ME
                 </h3>
             </div> */}
-             <div className="bg-[#36393f] text-white h-screen z-0">
+             <div className="bg-[#36393f] text-white h-screen z-0 overflow-y-scroll">
                 {/* Header */}
                 <Header />
                 <div className="flex relative text-center md:text-left md:flex-row max-w-7xl px-10 lg:pb-10 pb-10 justify-evenly mx-auto items-center">
@@ -99,11 +99,11 @@ const Home: NextPage = () => {
                                     Doenjang Jjigae / Brownies served hot with vanilla ice cream on top / Golden Kiwis / Brown rice, double chicken, black beans, tomato salsa, corn, fajitas, cheese, sour cream, lettuce, (sometimes guac), ask for a water cup and fill it with diet coke, take several spoons and forks.
                                 </p>
                         </div>
-                    </div>             
-             </div> 
-             <div className = "bg-black sticky bottom-0 text-white">
-                    <Footer />  
-            </div>     
+                    </div>  
+                    <div className = "bg-black sticky bottom-0 text-white">
+                        <Footer />  
+                    </div>            
+             </div>     
         </div>
     )
 }
