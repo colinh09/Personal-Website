7145f89c24191b967cb826b469946e383633500f
colinh09 <colin.hwang@cooper.edu>
2022-10-14 23:13:33 -0400

    please please please

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
 
 
                 {/* ----------------- DATA STRUCTURES AND ALGORITHMS ---------------------*/}
-                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 pb-10 justify-evenly mx-auto items-center">
+                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 pb-5 justify-evenly mx-auto items-center">
                     <h3 className="relative lg:top-5 top-2 uppercase tracking-[8px] text-gray-500 text-xl underline decoration-[#000000]/50">
                         Data Structures and Algorithms
                     </h3>
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
 
 
                 {/* ----------------- MACHINE LEARNING ---------------------*/}
-                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 pb-5 justify-evenly mx-auto items-center">
+                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 pb-20 justify-evenly mx-auto items-center">
                     <h3 className="relative top-24 uppercase tracking-[8px] text-gray-500 text-xl underline decoration-[#000000]/50">
                         Machine Learning
                     </h3>
