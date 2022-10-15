ca7ff1b191c1ea1bdf297f974c72ff3c050b3779
colinh09 <colin.hwang@cooper.edu>
2022-10-14 22:20:35 -0400

    please please please

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -27,8 +27,8 @@ function Contact({}: Props) {
     <div className = "h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
         <h3 className = "absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl">Contact Me</h3>
 
-        <div className = "flex flex-col space-y-3">
-            <h4 className = "text-4xl font-semibold text-center top-5">
+        <div className = "flex flex-col space-y-8">
+            <h4 className = "text-4xl font-semibold text-center">
                 Lets Talk!
             </h4>
 
