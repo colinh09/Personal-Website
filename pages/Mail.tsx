0f0a4335024ca0b65f1246b3487db38c3ae7db0f
colinh09 <colin.hwang@cooper.edu>
2022-10-14 21:34:47 -0400

    please please please

--- pages/Mail.tsx
+++ pages/Mail.tsx
@@ -7,13 +7,9 @@ import Header from '../components/Header';
 const Home: NextPage = () => {
     return (
         <div className="bg-[#36393f] text-white h-screen overflow-y-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#228C22]/80 snap-y snap-mandatory">
-            <section className="snap-start" id="hero">
-                <Header />
-            </section>
+            <Header />
 
-            <section className="snap-start" id="hero">
-                <Contact />
-            </section>
+            <Contact />
             <div className = "bg-black sticky bottom-0">
                     <Footer />  
             </div>   
--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
              <div className="bg-[#36393f] text-white h-screen overflow-y-scroll z-0 ">
                 {/* ----------------- HEADER ---------------------*/}
                 <Header />
-                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 pb-20 justify-evenly mx-auto items-center">
+                <div className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 lg:pb-10 pb-5 justify-evenly mx-auto items-center">
                     <h3 className="relative top-15 uppercase tracking-[20px] text-gray-500 text-3xl ">
                         PROJECTS
                     </h3>
