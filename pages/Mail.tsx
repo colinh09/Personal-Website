4bf9cb7ad28439a009e218c5bd26faa347b7d4c0
colinh09 <colin.hwang@cooper.edu>
2022-10-18 21:40:44 -0400

    lol

--- pages/Mail.tsx
+++ pages/Mail.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-[#36393f] text-white h-screen overflow-y-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#228C22]/80 snap-y snap-mandatory">
+        <div className="bg-[#36393f] text-white h-screen overflow-y-auto z-0">
             <Header />
 
             <Contact />
